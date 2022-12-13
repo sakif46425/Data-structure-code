05231a422e5f86eb20759a5cc86b69d5a056fb38
Sakif Khan <116105234+sakif46425@users.noreply.github.com>
2022-12-13 22:15:12 +0600

    Add files via upload

--- stack class link list.cpp
+++ stack class link list.cpp
@@ -1,1 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class stackNode
+{
+public:
+    int data;
+    stackNode * next;
+};
+stackNode * mewNode(int data)
+{
+    stackNode * stacknode = new stackNode ();
+    stacknode-> data = data;
+    stacknode-> next = NULL;
+
+    return stacknode;
+}
+
+int isEmpty (stackNode *root)
+{
+    return !root;
+}
+
+void push(stackNode * root, int new_data)
+{
+    stackNode
+}
+void push(stackNode** root, int new_data)
+{
+    stackNode* stackNode = newNode(new_data);
+    stackNode->next = *root;
+    *root = stackNode;
+    cout<<new_data<<endl;
+
+}
+
+int
